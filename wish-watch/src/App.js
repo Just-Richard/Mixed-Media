652f5e7ac31d5568b5802eecaadb47c632fc0359
Richard Thompson <rthompson0110@gmail.com>
2025-04-21 18:30:19 -0400

    Basic Setup
    Created watchlist, watched, and add pages with navigation between them.

--- wish-watch/src/App.js
+++ wish-watch/src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Add } from "./components/Add";
+import { Header } from "./components/Header";
+import { Watched } from "./components/Watched";
+import { Watchlist } from "./components/Watchlist";
 import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
 
 function App() {
-  return <h1> Hello World! </h1>;
+  return (
+    <Router>
+      <Header></Header>
+
+      <Routes>
+        <Route exact path="/" element={<Watchlist />} />
+
+        <Route path="/watched" element={<Watched />} />
+
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- wish-watch/src/components/Header.js
+++ wish-watch/src/components/Header.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">
+            <Link to="/">Wish & Watch</Link>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Watch List</Link>
+            </li>
+
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+
+            <li>
+              <Link to="/add" className="btn">
+                + Add
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
